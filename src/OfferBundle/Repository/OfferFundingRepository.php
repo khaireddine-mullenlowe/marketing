658c59b521fd522b63dd0297450b3647259ea6a3
Raldo CHEA <raldo@chea.io>
2018-06-22 15:50:35 +0200

    Get collection offer by criteria (range, model)

--- src/OfferBundle/Repository/OfferFundingRepository.php
+++ src/OfferBundle/Repository/OfferFundingRepository.php
@@ -10,5 +10,38 @@ use Doctrine\ORM\EntityRepository;
  */
 class OfferFundingRepository extends EntityRepository
 {
+    /**
+     * @param array $params
+     * @param bool $mostRecent
+     * @param bool $isActif
+     * @return \Doctrine\ORM\QueryBuilder
+     */
+    public function findByCustomFilters(array $params, bool $mostRecent = false, bool $isActif = true)
+    {
 
+        $queryBuilder = $this->createQueryBuilder('funding');
+        if (isset($params['model'])) {
+            $queryBuilder
+                ->andWhere('funding.modelId = :modelId')
+                ->setParameter('modelId', $params['model']);
+        } else if (isset($params['range'])) {
+            $queryBuilder
+                ->andWhere('funding.rangeId = :rangeId')
+                ->setParameter('rangeId', $params['range']);
+        }
+
+        if ($mostRecent) {
+            $queryBuilder->orderBy('funding.createAt', 'desc');
+        }
+
+        if ($isActif) {
+            $queryBuilder
+                ->andWhere('funding.status = 1')
+                ->andWhere('funding.startDate <= :today')
+                ->andWhere('funding.endDate >= :today')
+                ->setParameter('today', new \DateTime());
+        }
+
+        return $queryBuilder;
+    }
 }
