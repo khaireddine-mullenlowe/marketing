7a42a9dc0c3409d8b886ed770580157b47a091fe
Khaireddine GHANMI <khaireddine.ghanmi@mullenlowe.com>
2018-08-06 17:55:54 +0200

    Add endpoint for formId by names (or others criteria).

---
+++ src/MarketingBundle/Repository/Elastica/ContactFormRepository.php
@@ -0,0 +1,39 @@
+<?php
+namespace MarketingBundle\Repository\Elastica;
+
+use Elastica\Query\BoolQuery;
+use Elastica\Query\QueryString;
+use FOS\ElasticaBundle\Repository;
+
+class ContactFormRepository extends Repository
+{
+
+    /**
+     * Find one Contact form by criteria.
+     * @param array $criterias
+     * @return mixed
+     */
+    public function findOneBy(array $criterias)
+    {
+        if (empty($criterias)) {
+            throw new \InvalidArgumentException("Data not valid.");
+        }
+
+        $query = new BoolQuery();
+        $queryString = new QueryString();
+        foreach ($criterias as $field => $value) {
+            $queryString
+                ->setQuery(str_replace(" ", " AND ", trim($value)))
+                ->setDefaultField($field);
+            $query->addMust($queryString);
+        }
+
+        $result = $this->find($query);
+        if (is_array($result) && 1 === count($result)) {
+            return $result[0];
+        }
+
+        throw new \LogicException("Bad method call.");
+    }
+
+}
